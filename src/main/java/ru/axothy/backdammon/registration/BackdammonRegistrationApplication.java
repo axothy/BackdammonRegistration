40dfaf364cbd93b8c333f32271fcfc357bc9b237
globa <globatouareg@gmail.com>
2023-06-19 20:53:41 +0300

    Initial commit

---
+++ src/main/java/ru/axothy/backdammon/registration/BackdammonRegistrationApplication.java
@@ -0,0 +1,13 @@
+package ru.axothy.backdammon.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackdammonRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackdammonRegistrationApplication.class, args);
+	}
+
+}
